749d678778989990ce65b234c7177767356750f9
Llewellyn Falco <isidore@setgame.com>
2017-12-12 15:34:53 -0800

    ready to go

---
+++ FunctionalKata/FunctionalKata.cpp
@@ -0,0 +1,18 @@
+// FunctionalKata.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#define CATCH_CONFIG_MAIN
+#include "Catch.hpp"
+
+
+// Move lib/Catch
+//      test/Test
+//      test/main
+//      code/TrigMath
+
+// Include everything but main
+// Copy paste main into your main class
+// copy #include "stdafx.h" into test
+// delete main method
+// #define _USE_MATH_DEFINES to trigmath before math includes
---
+++ FunctionalKata/Test.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Catch.hpp"
+
+#include "TrigMath.h"
+
+using namespace std;
+
+
+
+TEST_CASE("Test Sin") {
+    TrigMath math;
+    REQUIRE(Approx(0.8676960543) == math.Sin(3.4));h
+}
